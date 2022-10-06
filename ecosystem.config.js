26bf1f202568f8199b21f3ce5b878c01fc337af4
Jakob Kruse <contact@jakob-kruse.de>
2022-10-06 20:19:08 +0200

    Add cron restart to PM2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,6 +5,7 @@ module.exports = {
 			script: 'pnpm start',
 			error_file: 'logs/err.log',
 			out_file: 'logs/out.log',
+			cron_restart: '0 */2 * * *',
 			time: false,
 			env: {
 				LOG_LEVEL: 'trace',
